48f488d523ebff0bc489d351627c7ddd383f96ec
Cian Butler <ciandevelops@gmail.com>
2022-08-08 14:17:18 -0400

    style vars in

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'title-text' : '#fffffe',
+      'paragraph-text' : '#abd1c6',
+      'button-color' : '#f9bc60',
+      'button-text' : '#001e1d',
+
+    },
     extend: {},
   },
   plugins: [],
