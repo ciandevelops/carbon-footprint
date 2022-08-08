c29c7628582912fbe3eba270b60eb1b2a79e2696
Cian Butler <ciandevelops@gmail.com>
2022-08-08 15:25:51 -0400

    styling issues

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Environment from "../assets/environment.svg";
 const Main = () => {
   return (
-    <main className="flex flex-col justify-center items-center h-screen p-4">
-      <div>
+    <main className="flex flex-col justify-center items-center max-w-[1240px] m-auto sm:flex-row-reverse h-screen p-4">
+      <div className="w-[50%] sm:h-[55%] p-8">
         <img src={Environment} alt="/" />
       </div>
 
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-title-text text-2xl py-4">MyCarbonFootprint.</h1>
-        <p className="text-paragraph-text py-4">
+      <div className="flex flex-col justify-center items-center sm:w-[50%] sm:h-[55%] sm:items-start">
+        <h1 className="text-title-text text-4xl py-4 text-bold">
+          MyCarbonFootprint.
+        </h1>
+        <p className="text-paragraph-text py-4 tracking-wide">
           [description here] Lorem ipsum dolor, sit amet consectetur adipisicing
           elit. Natus iste in soluta, suscipit tempore, neque aperiam ea
           consequuntur earum fugit enim quo, cumque praesentium reiciendis
@@ -17,8 +19,8 @@ const Main = () => {
           atque ducimus natus saepe deleniti neque. Rerum similique natus, nulla
           facere nostrum quaerat voluptate inventore doloremque! Nihil!
         </p>
-        <div className="py-4">
-          <button className="px-8 py-2 bg-button-color rounded-md text-button-text">
+        <div className="py-4 sm:py-8">
+          <button className="px-8 py-2 bg-button-color rounded-md sm:rounded-none text-button-text">
             Get Started
           </button>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,23 +8,23 @@ const Navbar = () => {
     setNav(!nav);
   };
   return (
-    <nav className="fixed left-0 top-0 w-full z-20">
+    <nav className="fixed left-0 top-0 w-full z-[50]">
       <div className="max-w-[1240px] m-auto p-4 flex justify-between items-center">
-        <div className="flex">
+        <div className="flex text-title-text">
           <h1 className="text-2xl">[Logo here?]</h1>
           <h1 className="text-2xl text-bold">MyCarbonFootprint</h1>
         </div>
 
         <div>
-          <ul className="hidden sm:flex">
-            <li className="p-4">Nav 1</li>
-            <li className="p-4">Nav 2</li>
-            <li className="p-4">Nav 3</li>
+          <ul className="hidden sm:flex text-title-text">
+            <li className="p-4 cursor-pointer">Nav 1</li>
+            <li className="p-4 cursor-pointer">Nav 2</li>
+            <li className="p-4 cursor-pointer">Nav 3</li>
           </ul>
           {/* MOBILE NAV */}
           <div
             onClick={handleNav}
-            className="block sm:hidden cursor-pointer z-10"
+            className="block sm:hidden cursor-pointer z-20 text-title-text"
           >
             {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
           </div>
@@ -32,18 +32,18 @@ const Navbar = () => {
             onClick={handleNav}
             className={
               nav
-                ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen text-center ease-in duration-300"
-                : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen text-center ease-in duration-300"
+                ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen text-center ease-in duration-300 bg-paragraph-text"
+                : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen text-center ease-in duration-300 bg-paragraph-text"
             }
           >
             <ul className="">
-              <li onClick={handleNav} className="p-4 text-4xl">
+              <li onClick={handleNav} className="p-4 text-4xl text-button-text">
                 <a href="#">Nav 1</a>
               </li>
-              <li onClick={handleNav} className="p-4 text-4xl">
+              <li onClick={handleNav} className="p-4 text-4xl text-button-text">
                 <a href="#">Nav 2</a>
               </li>
-              <li onClick={handleNav} className="p-4 text-4xl">
+              <li onClick={handleNav} className="p-4 text-4xl text-button-text">
                 <a href="#">Nav 3</a>
               </li>
             </ul>
